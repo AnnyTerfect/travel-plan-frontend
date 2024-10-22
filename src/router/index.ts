981fb06ecfce60773c4bc5eeec8beadcba6ef491
AnnyTerfect <1247470246@qq.com>
2024-10-22 23:20:16 +0800

    refactor: rename

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomePage from "@/views/HomeView.vue";
-import QuerySubmit from "@/views/QuerySubmitView.vue";
+import Home from "@/views/HomeView.vue";
+import Submit from "@/views/SubmitView.vue";
 import PlanOverall from "@/views/PlanOverallView.vue";
 import PlanDaily from "@/views/DetailView.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/", name: "Home", component: HomePage },
-    { path: "/query-submit", name: "QuerySubmit", component: QuerySubmit },
+    { path: "/", name: "Home", component: Home },
+    { path: "/query-submit", name: "QuerySubmit", component: Submit },
     {
       path: "/plan-overall/:id",
       name: "PlanOverall",
